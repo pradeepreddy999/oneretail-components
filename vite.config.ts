5e0163d8d74e8e239dd0112e6207c3604790fd61
Pradeep Reddy Palavalli <pradeepreddy.palavalli@inventiz.com>
2025-07-10 21:07:25 +0530

    initial commit.

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+/// <reference types="vitest/config" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [react(), dts(), tailwindcss()],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "OneRetail",
+      fileName: (format) => `oneretail.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
