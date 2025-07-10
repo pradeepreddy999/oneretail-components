6573ca00389882ed037bdd8af7c67bc02b205dea
pradeepreddy999 <pradeepreddy999@users.noreply.github.com>
2025-07-10 18:01:31 +0000

    deploy: aff9ecb3c5e71232d502d05fc157131718eed041

--- assets/Header.stories-Bz7NHhgR.js
+++
@@ -1,7 +0,0 @@
-import{H as a}from"./Header-DwrRShmD.js";import"./jsx-runtime-D_zvdyIk.js";import"./iframe-Dz8vk3DH.js";import"./Button-BYzdFwix.js";const{fn:r}=__STORYBOOK_MODULE_TEST__,m={title:"Example/Header",component:a,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:r(),onLogout:r(),onCreateAccount:r()}},e={args:{user:{name:"Jane Doe"}}},o={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    user: {
-      name: 'Jane Doe'
-    }
-  }
-}`,...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:"{}",...o.parameters?.docs?.source}}};const p=["LoggedIn","LoggedOut"];export{e as LoggedIn,o as LoggedOut,p as __namedExportsOrder,m as default};
