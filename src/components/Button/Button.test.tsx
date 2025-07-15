aa94374103ad40b63a4c8f98fe1446dcf7ce48e6
Jairam Chitra <jairam.chitra@inventiz.com>
2025-07-15 15:52:59 +0530

    styles checked

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -3,7 +3,9 @@ import Button from "./Button";
 
 test("render label Click Me", () => {
   const label = "Click Me";
-  const ButtonElement = <Button label={label} onClick={() => {}} />;
+  const ButtonElement = (
+    <Button label={label} variant="blue" onClick={() => {}} />
+  );
   render(ButtonElement);
   expect(screen.getByText("Click Me")).toBeInTheDocument();
 });
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,28 @@
-interface ButtonProps {
+// src/components/Button.tsx
+
+import React from "react";
+
+export interface ButtonProps {
   label: string;
   onClick: () => void;
+  variant: "blue" | "green" | "red";
 }
 
-const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
-  return <button onClick={onClick}>{label}</button>;
+const variantClasses: Record<ButtonProps["variant"], string> = {
+  blue: "bg-blue-700",
+  green: "bg-green-700",
+  red: "bg-red-700",
+};
+
+const Button: React.FC<ButtonProps> = ({ label, onClick, variant }) => {
+  return (
+    <button
+      className={`${variantClasses[variant]} px-4 py-2 rounded`}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
 };
 
 export default Button;
